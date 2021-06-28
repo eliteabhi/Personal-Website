7cbfdb38412b0f81f20223444dac1a50e3379b4c
Abhi Rangarajan <abhirangarajan2003@gmail.com>
2021-06-28 09:19:13 -0500

    wah

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,23 @@ use rocket_dyn_templates::Template;
 
 mod index;
 
+#[derive(serde::Serialize)]
+struct Message {
+    user: &'static str,
+    body: &'static str
+}
+
 #[get("/")] //Index of Website
 fn ind() -> Template {
-    Template::render("layout", "hi")
+    Template::render("index", &BoardContext {
+        current_user: Some("hi"),
+        messages: vec![Message{user: "userA", body: "This is the first test message."},
+                        Message{user: "userB", body: "This is the second test message."}],
+        parent: "layout"
+    })
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().attach(Template::fairing())
+    rocket::build().attach(Template::fairing()).mount("/", routes![ind])
 }
