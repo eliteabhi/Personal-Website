8600072026c8d7022385d0839a1e4742d1ad97c7
Abhi Rangarajan <abhirangarajan2003@gmail.com>
2021-06-28 20:43:52 -0500

    CSS

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,20 @@
 extern crate rocket_dyn_templates;
 
 use rocket_dyn_templates::Template;
+use std::fs;
+use rocket::http::ContentType;
 // use rocket::response::Redirect;
 
 #[launch]
-fn rocket() -> _ { //Launch website
+fn rocket() -> _ { // Launch website
 
-    rocket::build().attach(Template::fairing()).mount("/", routes![index])
+    rocket::build()
+                .attach(Template::fairing())
+                .mount("/", routes![index, css])
+
 }
 
-//Structs/Contexts
+// Structs/Contexts
 #[derive(serde::Serialize)]
 struct IndexContext {
     date: &'static str,
@@ -19,16 +24,24 @@ struct IndexContext {
     parent: &'static str
 }
 
-//Index(Default) page
+// Index(Default) page
 #[get("/")]
 fn index() -> Template {
 
-    Template::render("layout", &IndexContext {
+    Template::render("index", &IndexContext {
 
         date: "1/21/2001",
         name: "Abhi Rangarajan",
 
         parent: "layout"
     })
+    
+}
+
+// CSS
+#[get("/css/index.css")]
+fn css() -> (ContentType, &'static str) {
+
+    (ContentType::CSS, std::include_str!("../css/index.css"))
 
 }
