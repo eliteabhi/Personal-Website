bca7bfec618bfc7d2dc3434196294b90bb48a196
Abhi Rangarajan <abhirangarajan2003@gmail.com>
2021-06-30 15:23:05 -0500

    fixed text animation

--- src/Js/load_animation.js
+++ src/Js/load_animation.js
@@ -1,59 +1,63 @@
 
 // function([string1, string2],target id,[color1,color2])    
-consoleText(['Loading...'], 'text',['#FE433C','#3C50B1']);
+consoleText(['Loading...', 'Loading...', 'Loading...', 'Done!'], 'text',['#FE433C','#3C50B1']);
 
 function consoleText(words, id, colors) {
-  
-  var extra_css = " padding-left: 30px;";
 
-  if (colors === undefined) colors = ['#fff'];
-  var visible = true;
-  var con = document.getElementById('console');
-  var letterCount = 1;
-  var x = 1;
-  var waiting = false;
-  var target = document.getElementById(id)
-  target.setAttribute('style', 'color:' + colors[0] + "; " + extra_css);
-  window.setInterval(function() {
+var extra_css = " padding-left: 30px;";
 
-    if (letterCount === 0 && waiting === false) {
-      waiting = true;
-      target.innerHTML = words[0].substring(0, letterCount)
-      window.setTimeout(function() {
-        var usedWord = words.shift();
-        words.push(usedWord);
-        x = 1;
-        target.setAttribute('style', 'color:' + colors[0] + "; " + extra_css)
-        letterCount += x;
+if (colors === undefined) colors = ['#fff'];
+var visible = true;
+var con = document.getElementById('console');
+var letterCount = 1;
+var x = 1;
+var waiting = false;
+var target = document.getElementById(id)
+let num = 0;
+target.setAttribute('style', 'color:' + colors[0] + "; " + extra_css);
+window.setInterval(function() {
+
+
+    if (letterCount === 0 && waiting === false && num != words.length) {
+        waiting = true;
+        target.innerHTML = words[0].substring(0, letterCount)
+        window.setTimeout(function() {
+            var usedWord = words.shift();
+            words.push(usedWord);
+            x = 1;
+            target.setAttribute('style', 'color:' + colors[0] + "; " + extra_css)
+            letterCount += x;
         waiting = false;
         
-      }, 1000)
+        }, 1000)
     } 
-    
+
     else if (letterCount === words[0].length + 1 && waiting === false) {
-      waiting = true;
-      window.setTimeout(function() {
-        x = -1;
-        letterCount += x;
-        waiting = false;
-        target.setAttribute('style', 'color:' + colors[0] + "; " + extra_css)
-      }, 2010)
-      target.setAttribute('style', 'color:' + colors[1] + "; " + extra_css);
+        if (num < words.length-1) {
+            waiting = true;
+            window.setTimeout(function() {
+                x = -1;
+                letterCount += x;
+                waiting = false;
+                target.setAttribute('style', 'color:' + colors[0] + "; " + extra_css)
+        }, 2010)
+    }
+        target.setAttribute('style', 'color:' + colors[1] + "; " + extra_css); num++;
     } 
     else if (waiting === false) {
-      target.innerHTML = words[0].substring(0, letterCount)
-      letterCount += x
+        target.innerHTML = words[0].substring(0, letterCount)
+        letterCount += x
     }
-  }, 120)
+    }, 120)
 
-  window.setInterval(function() { // Bar
+    window.setInterval(function() { // Bar
     if (visible === true) {
-      con.className = 'console-bar hidden'
-      visible = false;
+        con.className = 'console-bar hidden'
+        visible = false;
 
     } else {
-      con.className = 'console-bar'
-      visible = true;
+        con.className = 'console-bar'
+        visible = true;
     }
-  }, 400)
+    }, 400)
 }
