7e5a1f739907b274e47b551acae36b4348d5cb55
eliteabhi <abhirangarajan2003@gmail.com>
2021-12-17 15:16:14 -0600

    basic webpage setup

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod pages_rust {
     pub mod about;
     pub mod error;
     pub mod load;
+    pub mod contact;
 }
 
 use pages_rust::*;
@@ -29,10 +30,11 @@ fn rocket() -> _ {
                 .register("/", catchers![error::not_found, error::my_prob])
 
                 //Pages
-                .mount("/", routes![index::index, global_css, error::error_css, index::index_css, about::about_css, load::load_css, favicon]) //Global
+                .mount("/", routes![index::index, global_css, error::error_css, index::index_css, about::about_css, load::load_css, contact::contact_css, favicon]) //Global
 
                 .mount("/loading...", routes![load::load, load::load_animation, global_css, load::load_css, favicon]) //Initial loading page
                 .mount("/about", routes![about::about, global_css, about::about_css, favicon]) // About page
+                .mount("/contact", routes![contact::contact, global_css, contact::contact_css, favicon]) // Contact page
 
 }
 
